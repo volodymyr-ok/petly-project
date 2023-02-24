2b563013e856d4b48be6dc21cc3073c724a670e3
Volodymyr Ok <v.shark.t@gmail.com>
2023-02-24 21:22:21 +0200

    fixed news route

--- src/redux/news/news-operations.js
+++ src/redux/news/news-operations.js
@@ -3,7 +3,7 @@ import { PublicApi } from "../../http/http";
 
 export const getNews = createAsyncThunk("/news", async (_, thunkAPI) => {
   try {
-    const res = await PublicApi.get("/news");
+    const res = await PublicApi.get("/api/news");
     return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
