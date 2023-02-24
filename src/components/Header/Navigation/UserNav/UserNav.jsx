e263fd37d8b4e0ee90f9692d9203c744f0c41026
Volodymyr Ok <v.shark.t@gmail.com>
2023-02-24 11:34:13 +0200

    added routes in header

--- src/components/Header/Navigation/UserNav/UserNav.jsx
+++ src/components/Header/Navigation/UserNav/UserNav.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import {BurgerBtn, BurgerImage} from "../../Header.styled";
-import burgerBtn from "../../../../assets/svg/burger-menu-btn.svg";
-import {NavItem, NavMain} from "../Nav/Nav.styled";
+import React from "react";
+import { NavItem, NavMain } from "../Nav/Nav.styled";
 
 const UserNav = () => {
-    return (
-        <NavMain>
-            <NavItem to="./user">User</NavItem>
-        </NavMain>
-    );
+  return (
+    <NavMain>
+      <NavItem to="./user">User</NavItem>
+    </NavMain>
+  );
 };
 
 export default UserNav;
