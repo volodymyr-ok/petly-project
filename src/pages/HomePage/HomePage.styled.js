ab1b8ad5eeb6e71b0a78d0620cc765ed74f7c430
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-02-25 16:08:18 +0200

    fix home page

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -16,11 +16,15 @@ import Union from "../../img/HomePage/Union.png";
 import Union2x from "../../img/HomePage/Union@2x.png";
 
 export const Title = styled.h1`
-  max-width: 280px;
-  font-size: 32px;
+  max-width: 283px;
+  font-size: 31px;
   line-height: 1.38;
   font-weight: 700;
   color: black;
+  @media screen and (min-width: 324px) {
+    font-size: 32px;
+  }
+
   @media screen and (min-width: 768px) {
     max-width: 588px;
     font-size: 68px;
@@ -29,23 +33,24 @@ export const Title = styled.h1`
 `;
 
 export const HomeSection = styled.section`
-  padding-top: 60px;
-  min-height: 543px;
-  /* height: calc(100vh - 74px); */
+  padding-top: 56px;
+  /* min-height: 543px; */
+  min-height: calc(100vh - 72px);
   background-repeat: no-repeat;
   background-position: bottom;
   background-image: url(${portraitM}), url(${BgM});
-  background-size: 320px 337px, 100vw 470px;
+  background-size: 324px 339px, 100vw 470px;
   @media (min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
     background-image: url(${portraitM2x}), url(${BgM2x});
   }
   @media screen and (min-width: 768px) {
-    min-height: 1108px;
+    /* min-height: 1108px; */
     padding-top: 88px;
     background-image: url(${portraitT}), url(${BgT});
-    background-size: 645px 715px, 100vw 1033px;
+    /* background-size: 645px 715px, 100vw 1033px; */
+    background-size: 645px 715px, 100vw calc(100vh - 116px);
     @media (min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
@@ -55,7 +60,7 @@ export const HomeSection = styled.section`
   @media screen and (min-width: 1280px) {
     padding-top: 92px;
     /* padding-bottom: 408px; */
-    min-height: calc(100vh - 64px);
+    /* min-height: calc(100vh - 64px); */
     background-image: url(${portraitD}), url(${BgD}), url(${Union});
     background-size: 590px 640px, 100vw calc(100vw * 0.375), 92px 89px;
     background-position-x: calc(50% + 330px), center, calc(50% + 118px);
@@ -65,5 +70,8 @@ export const HomeSection = styled.section`
       (min-resolution: 2dppx) {
       background-image: url(${portraitD2x}), url(${BgD2x}), url(${Union2x});
     }
+  }
+  & > div {
+    margin: 0;
   }
 `;
