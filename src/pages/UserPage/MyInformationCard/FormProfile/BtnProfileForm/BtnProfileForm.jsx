0d2a4702c47175e3da9132eeea3d06c6f7cbdd7d
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-02-27 17:16:52 +0200

    added functin

--- src/pages/UserPage/MyInformationCard/FormProfile/BtnProfileForm/BtnProfileForm.jsx
+++ src/pages/UserPage/MyInformationCard/FormProfile/BtnProfileForm/BtnProfileForm.jsx
@@ -1,16 +1,60 @@
 import Pencil from "../../../../../assets/svg/pencil.svg";
+import BlackPencil from "../../../../../assets/svg/black-pencil.svg";
 import { BtnPencil } from "./BtnProfileForm.styled";
+import ok from "../../../../../assets/svg/btn-ok.svg";
+import { useEffect, useState } from "react";
 
-export const BtnProfileForm = ({ onClick, disableInput, name }) => {
+export const BtnProfileForm = ({
+  onClick,
+  disableInput,
+  name,
+  disableTrue,
+  activeImg,
+}) => {
+  const [nameActivePancil, setNameActivePancil] = useState("");
+  const [colorPencil, setColorPencil] = useState(Pencil);
   const handleInput = (e) => {
     e.preventDefault();
-    console.log(e.currentTarget);
-    return onClick(!disableInput);
+
+    setNameActivePancil(e.currentTarget.name);
+    switch (e.currentTarget.name) {
+      case "name":
+        onClick({
+          ...disableTrue,
+          name: !disableInput.name,
+        });
+        break;
+      case "email":
+        onClick({
+          ...disableTrue,
+          email: !disableInput.email,
+        });
+        break;
+      case "birthday":
+        onClick({ ...disableTrue, birthday: !disableInput.birthday });
+        break;
+      case "phone":
+        onClick({ ...disableTrue, phone: !disableInput.phone });
+        break;
+      case "city":
+        onClick({ ...disableTrue, city: !disableInput.city });
+        break;
+      default:
+        break;
+    }
+    Object.values(disableInput).forEach((el) => {
+      if (el) {
+        return setColorPencil(Pencil);
+      } else {
+        return setColorPencil(BlackPencil);
+      }
+    });
   };
 
+  console.log(colorPencil);
   return (
     <BtnPencil onClick={handleInput} name={name}>
-      <img src={Pencil} alt="pencl" />
+      <img src={activeImg ? colorPencil : ok} alt="pencl" />
     </BtnPencil>
   );
 };
