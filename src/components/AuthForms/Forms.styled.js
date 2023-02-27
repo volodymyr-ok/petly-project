1f1426303527933003f76fa8d0b0db946d7aa3af
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-02-27 09:38:06 +0200

    goggle

--- src/components/AuthForms/Forms.styled.js
+++ src/components/AuthForms/Forms.styled.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Form, Field } from "formik";
 import { colors } from "../../utils/colors";
@@ -10,6 +10,7 @@ import bgLoginMob from "../../img/bgLoginMob.png";
 import { SectionTag } from "../Section/Section.styled";
 import showEye from "../../assets/svg/eye-show.svg";
 import hideEye from "../../assets/svg/eye-hide.svg";
+import { device } from "../../utils/mixin";
 
 export const Icon = styled.span`
   position: absolute;
@@ -37,14 +38,12 @@ export const AuthSection = styled(SectionTag)`
     height: 100vh;
   } */
   @media screen and (min-width: 768px) {
-    padding-top: 168px;
-    /* padding-bottom: 250px; */
+    padding-top: 100px;
     background-image: url(${bgLoginTab});
     min-height: calc(100vh - 75px);
   }
   @media screen and (min-width: 1280px) {
     padding-top: 80px;
-    /* padding-bottom: 100px; */
     background-image: url(${bgLoginDesc});
   }
 `;
@@ -59,8 +58,48 @@ export const ErrorText = styled.p`
   color: red;
 `;
 
+export const Text = styled.p`
+  font-size: 14px;
+  text-align: center;
+  margin-top: 20px;
+  ${device.tablet} {
+    font-size: 16px;
+  }
+`;
+
 export const Label = styled.label`
   position: relative;
+`;
+export const LoginGoogle = styled.a`
+  color: ${colors.black};
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  padding: 6px 28px;
+  border-radius: 40px;
+  font-size: 14px;
+  text-decoration: none;
+  border: 2px solid ${colors.accentOrange};
+  margin: 0 auto;
+  max-width: 300px;
+  transition: color 300ms linear, background-color 300ms linear;
+  :hover,
+  :focus {
+    background-color: ${colors.accentOrange};
+    color: ${colors.white};
+  }
+  @media (min-width: 768px) {
+    padding: 10px 48px;
+  }
+
+  svg {
+    width: 18px;
+    height: 18px;
+    margin-right: 5px;
+    position: relative;
+    z-index: 1;
+  }
 `;
 
 export const IconBox = styled.div`
@@ -164,7 +203,7 @@ export const SearchInput = styled(Input)`
 `;
 
 export const Paragraph = styled.p`
-  margin-top: 40px;
+  margin-top: 30px;
   text-align: center;
   & span {
     font-family: "Manrope";
