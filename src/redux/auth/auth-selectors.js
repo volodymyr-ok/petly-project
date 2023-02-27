05e975b6f29449fc9aa46833ed59aed040f1693a
Volodymyr Ok <v.shark.t@gmail.com>
2023-02-27 11:08:36 +0200

    .

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,5 +1,5 @@
 export const selectIsAuth = (state) => state.auth.isAuth;
 export const selectToken = (state) => state.auth.token;
 export const selectIsLoading = (state) => state.auth.isLoading;
-export const selectUser = (state) => state.auth.user.user;
-export const selectUsername = (state) => state.auth.user.user.name;
+export const selectUser = (state) => state.auth.user?.user;
+export const selectUsername = (state) => state.auth.user?.user?.name;
