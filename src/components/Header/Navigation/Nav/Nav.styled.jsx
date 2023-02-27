c4715b2f3f2e4617051e420e9ae6dd4c57452984
Andrii Drobotyuk <adrobotyuk@gmail.com>
2023-02-27 14:37:59 +0600

    header fix done

--- src/components/Header/Navigation/Nav/Nav.styled.jsx
+++ src/components/Header/Navigation/Nav/Nav.styled.jsx
@@ -2,7 +2,6 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { device } from "../../../../utils/mixin";
 import { colors } from "../../../../utils/colors";
-import logo from "../../../../assets/svg/accoun-logo.svg";
 
 export const NavMain = styled.nav`
   display: none;
@@ -28,11 +27,6 @@ export const NavUl = styled.ul`
     flex-direction: row;
     align-items: center;
   }
-`;
-
-export const NavAuthUl = styled.ul`
-  display: flex;
-  gap: 12px;
 `;
 
 export const BurgerContainer = styled.div`
@@ -81,46 +75,13 @@ export const NavItem = styled(NavLink)`
     text-decoration: underline;
   }
 
-  &:active {
-    color: ${colors.darkOrange};
+  &.active {
+    color: ${colors.accentOrange};
+    text-decoration: underline;
   }
 `;
 
-export const NavUserItem = styled(NavLink)`
-  display: ${(props) => (props.type ? `none` : "flex")};
-  color: ${colors.white};
-  padding: 10px 37px;
-  border-radius: 40px;
-  font-size: 16px;
-  text-decoration: none;
-  background: ${colors.accentOrange};
 
-  ${device.tablet} {
-    display: ${(props) => (props.type ? `flex` : "none")};
-    margin-right: 14px;
-    padding: 10px 28px;
-  }
-
-  ${device.desktop} {
-    margin-right: 0;
-  }
-
-  &::before {
-    content: ${(props) => (props.open ? `url(${logo})` : "")};
-    margin-right: 15px;
-    width: 23px;
-    height: 23px;
-  }
-
-  &:hover,
-  &:focus {
-    background: ${colors.darkOrange};
-  }
-
-  &:active {
-    // color: ${colors.darkOrange};
-  }
-`;
 
 export const BurgerMenu = styled.div`
   display: ${(props) => (props.open ? "block" : "none")};
