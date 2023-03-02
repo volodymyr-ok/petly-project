9f31fddc07f22f862e9dda02f929e05803f1c614
Dimon1989k <106411942+Dimon1989k@users.noreply.github.com>
2023-03-02 15:56:39 +0200

    change styles

--- src/components/PetsData/BtnAddPet/BtnAddPet.styled.jsx
+++ src/components/PetsData/BtnAddPet/BtnAddPet.styled.jsx
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const AddPetBtn = styled.button`
-  padding: 0;
+  width: 40px;
   height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: transparent;
-  overflow: hidden;
+  background-color: rgba(245, 146, 86, 1);
+  border-radius: 40px;
   border: none;
-  span {
-    margin-right: 15px;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 1.35;
-  }
-  img {
-    width: 40px;
+  cursor: pointer;
+
+  &:hover {
+    filter: drop-shadow(0px 4px 14px rgba(0, 0, 0, 0.11));
+    box-shadow: 0px 0px 6px 1px rgba(0, 0, 0, 0.11);
+    transition: transform 300ms linear;
+    transform: scale(1.05);
+    border: 2px solid #ffffff;
   }
 `;
